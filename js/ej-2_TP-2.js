7175b2cb40edc76f8eb4c9850455adfd3f92315e
Luciano Angeleri <lcnang@gmail.com>
2023-03-09 02:56:48 -0300

    Se resolvió el ejercicio 2 del Trabajo Practico N°2

--- js/ej-2_TP-2.js
+++ js/ej-2_TP-2.js
@@ -14,3 +14,31 @@ Si ingreso un número que no esté dentro del rango de 0 a 10
 mostrar un mensaje de “número erróneo”. Si el número ingresado no es válido 
 mostrar el mensaje “Introduce un número válido”.
 */
+let nota;
+do {
+  nota = parseFloat(prompt("Ingrese su calificación:"));
+  if (nota === "" || isNaN(nota) || nota < 0 || nota > 10){
+    alert("Ingresó una calificación erróneo");
+    alert("Ingrese un número válido");
+  }
+} while (nota === "" || isNaN(nota) || nota < 0 || nota > 10);
+switch (true) {
+    case nota >= 0 && nota <= 2.5:
+      document.write(`<p class="text-center text-danger fs-2">Su calificación es ${nota}, es MUY DEFICIENTE</p>"`);
+      break;
+    case nota > 2.5 && nota <= 4.5:
+      document.write(`<p class="text-center text-warning fs-2">Su calificación es ${nota}, es INSUFICIENTE</p>"`);
+      break;
+    case nota > 4.5 && nota <= 6.5:
+      document.write(`<p class="text-center text-success fs-2">Su calificación es ${nota}, es SUFICIENTE</p>"`);
+      break;
+    case nota > 6.5 && nota <= 8.5:
+      document.write(`<p class="text-center text-success fs-2">Su calificación es ${nota}, es BUENO</p>"`);
+      break;
+    case nota > 8.5 && nota <= 9.5:
+      document.write(`<p class="text-center text-success fs-2">Su calificación es ${nota}, es NOTABLE</p>"`);
+      break;
+    case nota > 9.5 && nota <= 10:
+      document.write(`<p class="text-center text-success fs-2">Su calificación es ${nota}, es SOBRESALIENTE</p>"`);
+      break;
+  }
