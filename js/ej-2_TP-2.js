1a163abeb43b8b45362406591d580a0fa3c0697f
Luciano Angeleri <lcnang@gmail.com>
2023-03-10 20:22:33 -0300

    Se modificó el tamaño de la letra en el enunciado

--- js/ej-2_TP-2.js
+++ js/ej-2_TP-2.js
@@ -24,21 +24,27 @@ do {
 } while (nota === "" || isNaN(nota) || nota < 0 || nota > 10);
 switch (true) {
     case nota >= 0 && nota <= 2.5:
-      document.write(`<p class="text-center text-danger fs-2">Su calificación es ${nota}, es MUY DEFICIENTE</p>"`);
+      document.write(`<h5 class="display-3 text-center">Resultado</h5>"`);
+      document.write(`<p class="text-center text-danger fs-1">Su calificación es ${nota}, es MUY DEFICIENTE</p>"`);
       break;
-    case nota > 2.5 && nota <= 4.5:
-      document.write(`<p class="text-center text-warning fs-2">Su calificación es ${nota}, es INSUFICIENTE</p>"`);
+      case nota > 2.5 && nota <= 4.5:
+      document.write(`<h5 class="display-3 text-center">Resultado</h5>"`);
+      document.write(`<p class="text-center text-warning fs-1">Su calificación es ${nota}, es INSUFICIENTE</p>"`);
       break;
-    case nota > 4.5 && nota <= 6.5:
-      document.write(`<p class="text-center text-success fs-2">Su calificación es ${nota}, es SUFICIENTE</p>"`);
+      case nota > 4.5 && nota <= 6.5:
+      document.write(`<h5 class="display-3 text-center">Resultado</h5>"`);
+      document.write(`<p class="text-center text-success fs-1">Su calificación es ${nota}, es SUFICIENTE</p>"`);
       break;
-    case nota > 6.5 && nota <= 8.5:
-      document.write(`<p class="text-center text-success fs-2">Su calificación es ${nota}, es BUENO</p>"`);
+      case nota > 6.5 && nota <= 8.5:
+      document.write(`<h5 class="display-3 text-center">Resultado</h5>"`);
+      document.write(`<p class="text-center text-success fs-1">Su calificación es ${nota}, es BUENO</p>"`);
       break;
-    case nota > 8.5 && nota <= 9.5:
-      document.write(`<p class="text-center text-success fs-2">Su calificación es ${nota}, es NOTABLE</p>"`);
+      case nota > 8.5 && nota <= 9.5:
+      document.write(`<h5 class="display-3 text-center">Resultado</h5>"`);
+      document.write(`<p class="text-center text-success fs-1">Su calificación es ${nota}, es NOTABLE</p>"`);
       break;
-    case nota > 9.5 && nota <= 10:
-      document.write(`<p class="text-center text-success fs-2">Su calificación es ${nota}, es SOBRESALIENTE</p>"`);
+      case nota > 9.5 && nota <= 10:
+      document.write(`<h5 class="display-3 text-center">Resultado</h5>"`);
+      document.write(`<p class="text-center text-success fs-1">Su calificación es ${nota}, es SOBRESALIENTE</p>"`);
       break;
   }
