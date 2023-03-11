5aa7d7f5b1f928aae3884c15fe5a5bbae6aa1965
Luciano Angeleri <lcnang@gmail.com>
2023-03-11 17:51:22 -0300

    Se resolvió el ejercicio 8 del TP2 de JS

--- js/ej-8_TP-2.js
+++ js/ej-8_TP-2.js
@@ -10,3 +10,20 @@ al número que indique el usuario (no mayor de 50)
 123456
 ……
 */
+let limite;
+do {
+  limite = parseInt(prompt("Ingrese un número (no mayor a 50)"));
+  if (limite > 50 || limite <= 0 || isNaN(limite)) {
+    alert("El número ingresado es inválido.\nIngrese un número del 1 al 50");
+  }
+} while (limite > 50 || limite <= 0 || isNaN(limite));
+
+document.write("<h5 class='display-5 text-center'>Resultado</h5>");
+for (let filas = 1; filas <= limite ; filas++) {
+    document.write("<p class='px-5 fs-5 text-primary'>")
+    for (let columnas = 1; columnas < filas; columnas++) {
+        document.write(`${columnas}`)
+    }
+    document.write(`${filas}`)
+    document.write("</p>");
+}
