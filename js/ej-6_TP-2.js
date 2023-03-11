48c91316b462cfab0036f118e9ea6ce11f2b23e2
Luciano Angeleri <lcnang@gmail.com>
2023-03-10 21:20:57 -0300

    Se le dio estilo al documento

--- js/ej-6_TP-2.js
+++ js/ej-6_TP-2.js
@@ -9,10 +9,12 @@ Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
 666666
 …….
 */
+document.write("<h5 class='display-5 text-center'>Resultado</h5>")
 for (let filas = 1; filas <= 30 ; filas++) {
+    document.write("<p class='px-5 fs-5 text-primary'>")
     for (let columnas = 1; columnas < filas; columnas++) {
         document.write(`${filas}`)
     }
     document.write(`${filas}`)
-    document.write(`<br>`)
+    document.write("</p>");
 }
