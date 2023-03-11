0eafdac382773760f380523decb1282091a78434
Luciano Angeleri <lcnang@gmail.com>
2023-03-10 21:17:01 -0300

    Se resolvió la pirámide

--- js/ej-6_TP-2.js
+++ js/ej-6_TP-2.js
@@ -9,3 +9,10 @@ Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
 666666
 …….
 */
+for (let filas = 1; filas <= 30 ; filas++) {
+    for (let columnas = 1; columnas < filas; columnas++) {
+        document.write(`${filas}`)
+    }
+    document.write(`${filas}`)
+    document.write(`<br>`)
+}
