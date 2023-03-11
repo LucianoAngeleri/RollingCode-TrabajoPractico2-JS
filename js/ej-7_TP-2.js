fb4cd700c0d812994d801df8b19b12a13e709f09
Luciano Angeleri <lcnang@gmail.com>
2023-03-11 17:40:15 -0300

    Se resolvió el ejercicio 7 del TP2 de JS

--- js/ej-7_TP-2.js
+++ js/ej-7_TP-2.js
@@ -12,3 +12,20 @@ número que indique el usuario (no mayor de 50)  de la siguiente forma :
 22
 1
 */
+let limite;
+do {
+  limite = parseInt(prompt("Ingrese un número (no mayor a 50)"));
+  if (limite > 50 || limite <= 0 || isNaN(limite)) {
+    alert("El número ingresado es inválido.\nIngrese un número del 1 al 50");
+  }
+} while (limite > 50 || limite <= 0 || isNaN(limite));
+
+document.write("<h5 class='display-5 text-center'>Resultado</h5>");
+for (let filas = limite; filas <= limite && filas > 0; filas--) {
+  document.write("<p class='px-5 fs-5 text-primary'>");
+  for (let columnas = 1; columnas < filas; columnas++) {
+    document.write(`${filas}`);
+  }
+  document.write(`${filas}`);
+  document.write("</p>");
+}
